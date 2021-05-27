97cb46dcec12c176e98e1e9fe7e47c320cf75679
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-05-27 15:26:27 -0400

    Added json handling and findOne to post request

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3001;
 
 const app = express()
 
+app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cors({optionsSuccessStatus: 200}));
 app.use(express.static(path.resolve(__dirname, '../client/build')));
@@ -21,20 +22,22 @@ const shortenSchema = new mongoose.Schema({
 const Shorten = mongoose.model('Shorten', shortenSchema);
 
 //connect to mongodb
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI_TEST, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 //post
 //verify that domain is valid
 //find if url exists in db already
 //return url+shorturl if it does
 //otherwise create new
-app.get('/api/shorturl', (req, res) => { 
+app.post('/api/shorturl', (req, res) => { 
     //post request will use req.body.domain
-    domain = "https://google.com";
+    const domain = req.body.domain;
+    console.log(domain);
     //filter input to fit domain.name format
     //remove http(s)://
-    domainRegex = /^https?:\/\//i;
-    domainFormat = domain.replace(domainRegex, "");
+    const domainRegex = /^https?:\/\//i;
+    const domainFormat = domain.replace(domainRegex, "");
     console.log(domainFormat);
     //verify the domain is valid
     dns.lookup(domainFormat, (err, address, family) => {
@@ -42,7 +45,24 @@ app.get('/api/shorturl', (req, res) => {
             res.json({error:"Invalid URL"});
         } 
     })
-    console.log("url is valid");
+    //does url already have shortlink?
+    Shorten.findOne({ long_url: domain }, function(err, domain){
+        if(err){
+            console.log(err);
+        } else {
+            if(domain === null){
+                //create new
+                console.log("did not find");
+                console.log(domain);
+
+            } else {
+                //return existing
+                
+            }
+        }
+    });
+    //return existing
+    //create new
     res.json({status:"all clear"})
 });
 
