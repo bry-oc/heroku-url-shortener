4bfe2b0968822aa60b0f35af50ccb76551515ee7
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-05-27 20:45:45 -0400

    Added form

--- client/src/components/formDisplay.js
+++ client/src/components/formDisplay.js
@@ -1,6 +1,22 @@
+import React from 'react';
+
 function FormDisplay(){
+    const [originalUrl , setOriginalUrl] = React.useState(null);
+    const [shortUrl, setShortUrl] = React.useState(null);
+
+    //user enters the original url and clicks submit
+    //text input will change to short url
+    //submit input will change to copy
+    //submit input will change to copied
+    //if text input is cleared, reset
     return(
-        <h1>Hello World</h1>
+        <div>
+            <form>
+                <label for="url">Please enter your URL</label><br></br>
+                <input type="text" id="url" placeholder="https://www.example.com"></input>
+                <input type="submit" value="Shorten"></input>
+            </form>
+        </div>        
     );
 }
 
