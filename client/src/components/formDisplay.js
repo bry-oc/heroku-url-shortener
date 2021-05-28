ad795c070ca6925df93cb2c57512312582f0cb40
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-05-27 22:14:28 -0400

    Added handling for text input and post for submit

--- client/src/components/formDisplay.js
+++ client/src/components/formDisplay.js
@@ -3,7 +3,43 @@ import React from 'react';
 function FormDisplay(){
     const [originalUrl , setOriginalUrl] = React.useState(null);
     const [shortUrl, setShortUrl] = React.useState(null);
+    const [display, setDisplay] = React.useState("Shorten");
+    const [text, setText] = React.useState("");
 
+    let createShortUrl = (e) => {
+        e.preventDefault();
+        let url = e.target.url.value;
+        console.log(url);
+        if(display === "Shorten"){
+            fetch('/api/shorturl', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    domain: url,
+                })
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    if(!data.error){
+                        console.log(data);
+                        setText(data.short_url);
+                    }
+                })
+        } else if (display === "Copy"){
+
+        } else if (display === "Copied"){
+
+        }
+    }
+
+    let handleChange = (e) =>{
+        setText(e.target.value)
+        if(e.target.value === ""){
+            setDisplay("Shorten");
+        }
+    }
     //user enters the original url and clicks submit
     //text input will change to short url
     //submit input will change to copy
@@ -11,10 +47,10 @@ function FormDisplay(){
     //if text input is cleared, reset
     return(
         <div>
-            <form>
+            <form onSubmit={createShortUrl}>
                 <label for="url">Please enter your URL</label><br></br>
-                <input type="text" id="url" placeholder="https://www.example.com"></input>
-                <input type="submit" value="Shorten"></input>
+                <input type="text" id="url" placeholder="https://www.example.com" onChange={handleChange} value={text}></input>
+                <input type="submit" value={display}></input>
             </form>
         </div>        
     );
